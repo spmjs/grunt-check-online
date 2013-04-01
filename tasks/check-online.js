b5227dd9cd675c610a78061b8f6b8302eec43464
Hsiaoming Yang <me@lepture.com>
2013-04-01 10:43:58 +0800

    ready to ship

--- tasks/check-online.js
+++ tasks/check-online.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
           filename = filepath;
           filepath = path.join(fileObj.cwd, filepath);
         } else {
-          filename = path.relative(fileObj.orig.cwd, filepath);
+          filename = path.relative(fileObj.orig.cwd || '', filepath);
         }
         distfiles.push(path.join(fileObj.dest, filename));
       });
